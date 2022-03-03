233c499a974ed93fccd2921c527aacb4b2a08fdc
andrewkuhl <andrewkuhlz@gmail.com>
2022-03-03 04:18:47 -0700

    Initial Commit

---
+++ passion_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  passion_project
+//
+//  Created by Andrew Kuhl on 3/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
